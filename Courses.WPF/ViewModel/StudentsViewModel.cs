5b25aeae2c2a8717358158c4308d251d5186899d
Oksana Barinova <novanasko@protonmail.com>
2025-02-26 10:21:48 +0200

    make students menu selectable

--- Courses.WPF/ViewModel/StudentsViewModel.cs
+++ Courses.WPF/ViewModel/StudentsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Courses.DAL.Data;
+using Courses.DAL.Model;
+using Courses.WPF.Command;
 using System.Collections.ObjectModel;
 
 namespace Courses.WPF.ViewModel
@@ -10,6 +12,8 @@ namespace Courses.WPF.ViewModel
         public StudentsViewModel(IStudentDataProvider studentDataProvider)
         {
             _studentDataProvider = studentDataProvider;
+            CreateCommand = new DelegateCommand(Create);
+            DeleteCommand = new DelegateCommand(Delete, CanDelete);
         }
         public ObservableCollection<StudentItemViewModel> Students { get; } = new();
         public StudentItemViewModel? SelectedStudent
@@ -20,9 +24,13 @@ namespace Courses.WPF.ViewModel
                 _selectedStudent = value;
                 RaisePropertyChanged();
                 RaisePropertyChanged(nameof(IsStudentSelected));
+                DeleteCommand.RaiseCanExecuteChanged();
             }
         }
         public bool IsStudentSelected => SelectedStudent is not null;
+
+        public DelegateCommand CreateCommand { get; }
+        public DelegateCommand DeleteCommand { get; }
 
         public override async Task LoadAsync()
         {
@@ -41,5 +49,23 @@ namespace Courses.WPF.ViewModel
                 }
             }
         }
+        private void Create(object? parameter)
+        {
+            var student = new Student { FirstName = "Name" };
+            var viewModel = new StudentItemViewModel(student);
+            Students.Add(viewModel);
+            SelectedStudent = viewModel;
+        }
+
+        private void Delete(object? parameter)
+        {
+            if (SelectedStudent is not null)
+            {
+                Students.Remove(SelectedStudent);
+                SelectedStudent = null;
+            }
+        }
+
+        private bool CanDelete(object? parameter) => SelectedStudent is not null;
     }
 }
